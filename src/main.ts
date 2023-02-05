33560c603931c45abd9454ca51ed6b91c33cf076
Cherepanov Kirill <tcherepanov.kir@gmail.com>
2023-02-05 16:26:37 +0300

    fix: check reviewer remark

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 import { ServerExceptionFilter } from './filters/server-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { ServerException } from './exceptions/server.exception';
 import { ErrorCode } from './exceptions/error-codes';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +27,7 @@ async function bootstrap() {
 
   app.enableCors();
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.use(limiter);
   app.use(helmet());
   app.useGlobalFilters(new ServerExceptionFilter());
 
